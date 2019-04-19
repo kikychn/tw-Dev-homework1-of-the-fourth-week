71c861463bbda08e14484050d2b3c063749b5986
kikychn <kikychn@gmail.com>
2019-04-19 15:04:34 +0800

    achieve the test for generate()

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,26 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    @Test
+    public void testGenerate() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        Answer answer = answerGenerator.generate();
+        assertEquals("1 2 3 4", answer.toString());
+    }
 }
 
